2beab7c42548a0ef001596838dab6595f60b5688
53rao <raox58@gmail.com>
2025-06-23 13:36:14 +0530

    Added Some folders and data to New.jsx

--- App_Template/src/Data/New.jsx
+++ App_Template/src/Data/New.jsx
@@ -4,7 +4,9 @@ const newshoeData = [
     name: "Air Boost Runner",
     price: 129.99,
     sizes: [6, 7, 8, 9, 10, 11],
-    colors: "Black",
+    colors: "blue",
+    colorStyle: "bg-blue-300",
+    productNumber: "P7384",
     image: "/Images/blue1.png"
   },
   {
@@ -12,7 +14,9 @@ const newshoeData = [
     name: "Trail Hawk GTX",
     price: 189.95,
     sizes: [8, 9, 10, 11, 12],
-    colors: ["Brown", "Black"],
+    colors: "purple",
+    colorStyle: "bg-purple-300",
+    productNumber: "P2057",
     description: "Waterproof hiking shoe designed for rugged terrain.",
     image: "/Images/purple.png"
   },
@@ -21,9 +25,61 @@ const newshoeData = [
     name: "Urban Flex High Top",
     price: 99.5,
     sizes: [7, 8, 9, 10, 12],
-    colors: ["Grey", "Navy", "Olive"],
+    colors: "cyan",
+    colorStyle: "bg-cyan-300",
+    productNumber: "P4892",
     description: "Trendy high-top sneaker with flexible outsole.",
     image: "/Images/cyan1.png"
+  },
+  {
+    Collection: "New",
+    name: "Sunset Cruiser",
+    price: 115.0,
+    sizes: [6, 7, 8, 9],
+    colors: "amber",
+    colorStyle: "bg-amber-300",
+    productNumber: "P9731",
+    image: ""
+  },
+  {
+    Collection: "New",
+    name: "Sky Runner",
+    price: 140.75,
+    sizes: [8, 9, 10],
+    colors: "sky",
+    colorStyle: "bg-sky-300",
+    productNumber: "P6629",
+    image: ""
+  },
+  {
+    Collection: "New",
+    name: "Forest Trekker",
+    price: 130.25,
+    sizes: [9, 10, 11],
+    colors: "emerald",
+    colorStyle: "bg-emerald-300",
+    productNumber: "P5216",
+    image: ""
+  },
+  {
+    Collection: "New",
+    name: "Volcano Sprint",
+    price: 155.0,
+    sizes: [7, 8, 9, 10],
+    colors: "red",
+    colorStyle: "bg-red-300",
+    productNumber: "P3745",
+    image: ""
+  },
+  {
+    Collection: "New",
+    name: "Ocean Glide",
+    price: 122.5,
+    sizes: [6, 7, 8, 9, 10],
+    colors: "indigo",
+    colorStyle: "bg-indigo-300",
+    productNumber: "P8152",
+    image: ""
   }
 ];
 
